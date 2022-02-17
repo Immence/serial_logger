0124d515f8a7184907470b1690a7d74fc709646d
jeppePB <andreas@plaato.io>
2022-02-17 16:22:17 +0100

    Made serial monitor read only

--- components/my_text_box.py
+++ components/my_text_box.py
@@ -3,6 +3,7 @@ from PySide6 import QtGui, QtWidgets
 class MyTextBox(QtWidgets.QTextEdit):
     def __init__(self):
         QtWidgets.QTextEdit.__init__(self)
+        self.setReadOnly(True)
         
     def keyPressEvent(self, e: QtGui.QKeyEvent) -> None:
         return super().keyPressEvent(e)
