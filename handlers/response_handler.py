7d2bcb81082c1811de6569e4d43319b0e3ce085a
jeppePB <andreas@plaato.io>
2022-05-05 16:14:06 +0200

    Fix response handler having access to csv writer

--- handlers/response_handler.py
+++ handlers/response_handler.py
@@ -3,7 +3,6 @@ from PySide6 import QtCore
 from datetime import datetime
 import time
 from bridges.program_state_bridge import ProgramStateBridge
-from util import csv_writer
 from util import log_writer
 
 unit_dict = {           # Fuck it, I might handle unit stuff at a later point if it's necessary.
@@ -51,8 +50,7 @@ class ResponseHandler(QtCore.QObject):
             parsed_text = self.interpret_response(self.qr_code, text)
             if "frequency" and "temperature" in parsed_text:
                 self._PSB.reading_received.emit(parsed_text)
-            csv_writer.write_csv_line(parsed_text)
-
+                
     def set_qr_code(self, qr_code: str) -> None:
         self.qr_code = qr_code
         self.handle_response(f"//////////////////////////////////\nQR-code has been set to {self.qr_code}")
