1baa3bacf34a36a9dd7c866d5ee2038e1163092b
jeppePB <andreas@plaato.io>
2022-05-11 15:08:57 +0200

    Add keyboard shortcuts for bottom dock menu

--- widgets/toolbars/bottom_toolbar.py
+++ widgets/toolbars/bottom_toolbar.py
@@ -15,19 +15,19 @@ class BottomToolBar(QtWidgets.QToolBar):
 
     def _create_actions(self, serial_monitor_action, line_graph_widget_action, scatter_graph_widget_action):
         self._serial_monitor_action = QtGui.QAction(self)
-        self._serial_monitor_action.setText("&Serial Monitor")
+        self._serial_monitor_action.setText("&Serial Monitor (F1)")
         self._serial_monitor_action.setIcon(Icons.test_icon())
         self._serial_monitor_action.triggered.connect(serial_monitor_action)
+        self._serial_monitor_action.setShortcut("F1")
 
         self._line_graph_widget_action = QtGui.QAction(self)
-        self._line_graph_widget_action.setText("&Line Graph widget")
+        self._line_graph_widget_action.setText("&Line Graph widget (F2)")
         self._line_graph_widget_action.setIcon(Icons.line_graph_icon())
         self._line_graph_widget_action.triggered.connect(line_graph_widget_action)
+        self._line_graph_widget_action.setShortcut("F2")
         
         self._scatter_graph_widget_action = QtGui.QAction(self)
-        self._scatter_graph_widget_action.setText("&Scatter plot widget")
+        self._scatter_graph_widget_action.setText("&Scatter plot widget (F3)")
         self._scatter_graph_widget_action.setIcon(Icons.scatter_plot_icon())
-        self._scatter_graph_widget_action.triggered.connect(scatter_graph_widget_action)
-
-
-    
+        self._scatter_graph_widget_action.triggered.connect(scatter_graph_widget_action)    
+        self._scatter_graph_widget_action.setShortcut("F3")
