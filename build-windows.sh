6b65cc00e5e60914866ba597f77d14ebd22fa3fe
jeppePB <andreas@plaato.io>
2022-06-13 21:55:53 +0200

    Add build script for windows executables!

--- build-windows.sh
+++ build-windows.sh
@@ -4,16 +4,16 @@
 # CONFIGURATION #
 #################
 script_name="main.py"
-program_name="PLAATO_Serial_Logger-unix"
+program_name="PLAATO_Serial_Logger-win"
 dist_dir="dist/${program_name}"
 #################
 
 # Recreate the dist directory and station directories
-rm -r ./dist
+rm -r $dist_dir
 mkdir -p $dist_dir
 mkdir "${dist_dir}/output"
 
 # Create a base executable file for the program
-pyinstaller --onefile --distpath $dist_dir --name "${program_name}" $script_name
+wine ~/.wine/drive_c/Python39/Scripts/pyinstaller.exe --onefile --distpath $dist_dir --name "${program_name}" $script_name
 # Copy over assets
 echo $dist_dir | xargs -n 1 cp -r ./files
